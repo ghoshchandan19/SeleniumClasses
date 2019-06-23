435c3c4f1008cb4a39afab319ea685ea1082fa82
Chandan Ghosh <ghoshchandan19@gmail.com>
2019-06-24 01:01:40 +0530

    Added POM

--- Sunday/src/package3/SeleniumPage.java
+++ Sunday/src/package3/SeleniumPage.java
@@ -1,5 +1,23 @@
 package package3;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import lombok.Getter;
+
 public class SeleniumPage {
+
+	private @Getter WebElement about;
+	private @Getter WebElement support;
+	private @Getter WebElement doc;
+	private @Getter WebElement download;
+	private @Getter WebElement projects;
+
+	public SeleniumPage(WebDriver driver) {
+		about = driver.findElement(By.xpath("//li[@id='menu_about']/a"));
+		support =driver.findElement(By.xpath("//li[@id='menu_support']/a"));
+
+	}
 
 }
